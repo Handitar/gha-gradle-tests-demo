e07593f15074c22d368551cd673b3764c46f3036
Kyrylo <mrhanditar@yahoo.com>
2026-05-06 13:18:44 +0200

    initial commit

---
+++ src/test/java/com/example/SampleTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SampleTest {
+
+    @Test
+    void sumWorks() {
+        assertEquals(4, 2 + 2);
+    }
+}
